d97ddb7ed4956efdc0b2edac2191ebb579d3955f
Seth Christmus <seth.christmus@fdmgroup.com>
2023-02-14 14:29:40 -0500

    tip calculation logic implemented

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Welcome to Tipsy, a free tip calculation tool.")
+
+total_bill = float(input("What was the total bill? "))
+percentage_tip = int(input("What percentage tip would you like to leave? 15, 18, or 20? "))
+number_of_people = int(input("How many people are splitting the bill? "))
+
+total_plus_tip = total_bill + (total_bill * (percentage_tip / 100))
+amount_owed_per_person = round(total_plus_tip / number_of_people, 2)
+
+print(amount_owed_per_person)
+
